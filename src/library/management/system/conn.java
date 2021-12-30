20a49af85819b158cb349e4bb39d50643662769c
anshika01-agr <anshikaagrawal141@gmail.com>
2021-12-30 18:45:05 +0530

    first commit

---
+++ src/library/management/system/conn.java
@@ -0,0 +1,21 @@
+package library.management.system;
+
+
+import java.sql.*;
+
+public class conn {
+    Connection c;
+    Statement s;
+    public conn(){
+    try{
+        Class.forName("com.mysql.jdbc.Driver");
+        c =DriverManager.getConnection("jdbc:mysql:///library","root","Anshika@123");
+        s =c.createStatement();
+        
+        
+    }
+    catch(Exception e){
+        System.out.println(e);
+    }
+    } 
+    }
